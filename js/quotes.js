e5c1970ec4306b827e5b28b89389a3028abfd18f
Garrett Trott <garretttrott@gmail.com>
2019-10-30 15:43:18 -0700

    adds partial qoutes, font style souces and html divs

---
+++ js/quotes.js
@@ -0,0 +1,49 @@
+// A collection of quotes to display to the viewer
+
+let quotes = [
+  {
+    quote:
+      "A man who procrastinates in his choosing will inevitably have his choice made for him by circumstance.",
+    source: "Hunter S. Thompson",
+    citation: "The Proud Highway: Saga of a Desperate Southern Gentleman",
+    year: "1955-1967"
+  },
+  {
+    quote:
+      "Today, a young man on acid realized that all matter is merely energy condensed to a slow vibration – that we are all one consciousness experiencing itself subjectively. There is no such thing as death, life is only a dream, and we are the imagination of ourselves. Here's Tom with the weather.",
+    source: "Bill Hicks",
+    citation: "Sane Man",
+    year: "1989"
+  },
+  {
+    quote:
+      "In our obscurity - in all this vastness - there is no hint that help will come from elsewhere to save us from ourselves. It is up to us.",
+    source: "Carl Sagan",
+    citation: "Pale Blue Dot",
+    year: "1990"
+  },
+  {
+    quote: "Turn on, tune in, drop out.",
+    source: "Timothy Leary",
+    citation: "Human Be-In",
+    year: "1967"
+  },
+  {
+    quote: "Inside every cynical person, there is a disappointed idealist.",
+    source: "George Carlin",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: ""
+  }
+];
